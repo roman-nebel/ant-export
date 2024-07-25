8d7316deda5adcdb10f527cdc3501cce9feb9d46
Roman Nebel <im.roman.nebel@gmail.com>
2024-07-25 15:22:18 +0200

    Add checkbox component

---
+++ src/plugin/components/components/checkbox.ts
@@ -0,0 +1,11 @@
+export const checkbox = {
+  el: 'Checkbox',
+  label: '',
+  attr: {
+    autoFocus: null,
+    checked: null,
+    defaultChecked: null,
+    disabled: null,
+    indeterminate: null,
+  },
+};
